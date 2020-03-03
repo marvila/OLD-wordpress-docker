2d3a8aaa523b2001a61d882ee67e10efcf75c240
Fred Marvila <marvila@sundaycooks.com>
2020-03-03 16:25:41 -0300

    improve post-setup.sh and add phpmyadmin details

--- post-setup.sh
+++ post-setup.sh
@@ -1,4 +1,16 @@
 #!/bin/bash -x
+#chown -R www-data:www-data wordpress
+#chmod -R 777 wordpress
+cp php.ini wordpress
+
+
+wget -O phpmyadmin.tar.gz https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-english.tar.gz
+tar xzvf phpmyadmin.tar.gz
+mv phpMyAdmin* wordpress/pma
+rm phpmyadmin.tar.gz
+cp config.inc.php wordpress/pma/
+mkdir -P wordpress/pma/uploads
+
 chown -R www-data:www-data wordpress
 chmod -R 777 wordpress
-cp php.ini wordpress
+chmod 660 wordpress/pma/config.inc.php
