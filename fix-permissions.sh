201114ea1f9f9134dc4dcf6ab22c80dd07433ae6
Fred Marvila <marvila@sundaycooks.com>
2021-04-27 11:35:03 -0300

    trying to fix permissions, but still commented

--- fix-permissions.sh
+++ fix-permissions.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 sudo chmod -R 777 wordpress
 sudo chown -R www-data:www-data wordpress
+
+# taken from link to try and avoid git pushing permissions
+# https://stackoverflow.com/questions/1580596/how-do-i-make-git-ignore-file-mode-chmod-changes
+# sudo find . -type d -exec chmod a+rwx {} \;
+# sudo find . -type f -exec chmod a+rw {} \;
