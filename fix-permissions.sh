6c7248dddc6bd0cc7ffb684c66adee3fc25e76fe
Fred Marvila <marvila@sundaycooks.com>
2021-04-27 11:45:25 -0300

    improve fix_permissions

--- fix-permissions.sh
+++ fix-permissions.sh
@@ -4,5 +4,12 @@ sudo chown -R www-data:www-data wordpress
 
 # taken from link to try and avoid git pushing permissions
 # https://stackoverflow.com/questions/1580596/how-do-i-make-git-ignore-file-mode-chmod-changes
+# You can ignore any errors about "chmod: missing operand after `+x'"
+#
+# !! EXECUTAR NO DIRETORIO DO GIT !!
+#sudo git diff --summary | grep --color 'mode change 100755 => 100644' | cut -d' ' -f7- | xargs -d'\n' chmod +x 
+#sudo git diff --summary | grep --color 'mode change 100644 => 100755' | cut -d' ' -f7- | xargs -d'\n' chmod -x 
+
+# Outro comando
 # sudo find . -type d -exec chmod a+rwx {} \;
 # sudo find . -type f -exec chmod a+rw {} \;
